3fda4c4a7f6bdcdb20ac62067ba185f414c7cb92
Glauco Gonçalves <glaestgon@gmail.com>
2016-10-16 17:00:11 -0300

    comunicacao com servico rapifire

--- ir-mqtt/config.h
+++ ir-mqtt/config.h
@@ -1,9 +1,9 @@
-const char* ssid = "******";
-const char* password = "******";
-const char* mqtt_server = "******";
-const int   mqtt_port = 1883;
-const char* mqtt_client = "******";
-const char* mqtt_usr = "******";
-const char* mqtt_psw = "******";
-const char* inTopic = "in";
+const char* ssid = "familia";
+const char* password = "daniamor#09$04";
+const char* mqtt_server = "m12.cloudmqtt.com";
+const int   mqtt_port = 15763;
+const char* mqtt_client = "esp";
+const char* mqtt_usr = "jualabs";
+const char* mqtt_psw = "Ju@L@bs2016";
+const char* inTopic = "deinfo/sala06/ac";
 const char* outTopic = "out";
---
+++ rapifire/umidade.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import paho.mqtt.client as mqtt
+import time
+import random
+
+mqttc = mqtt.Client("python_pub")
+mqttc.username_pw_set("EThoxloDv35yQYLHlKVPIb","S2hyEhOwdMGH")
+mqttc.connect("mqtt.rapifire.com", 1883)
+while True:
+   r = random.random()+17.31
+   mqttc.publish("/EThoxloDv35yQYLHlKVPIb/data", '{"e" : [{"n" : "temp", "v" : '+str(r)+'}]}')
+   mqttc.loop(5) #timeout = 5s
+   time.sleep(2)
